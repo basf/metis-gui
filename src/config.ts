ac79ef254f45eb0c30c809687ef4eb0d9be4ee90
Pavel Malyshev <paul.malys@gmail.com>
2021-07-20 12:35:16 +0300

    #24 - move base path value to config file

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,2 @@
 export const API_BASEURL = 'https://peer.basf.science';
+export const BASE_PATH = __env === 'production' ? '/gui' : '';
--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import { prefs } from 'svelte-pathfinder';
 
 import '@/assets/global.css';
 
+import { BASE_PATH } from '@/config';
+
 import App from '@/App.svelte';
 
 prefs.array.format = 'separator';
-prefs.basePath = __env === 'production' ? '/gui' : '';
+prefs.basePath = BASE_PATH;
 
 const app = new App({
     target: document.body,
