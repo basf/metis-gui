8a43db44d33baeec04b5c8a13fc7b5c6affef53a
Pavel Malyshev <paul.malys@gmail.com>
2021-08-04 18:22:25 +0300

    change SSE stream URL because /data has taken by REST API now

--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -1,6 +1,6 @@
 import { streamable } from 'svelte-streamable';
 
 export default streamable<number>({
-    url: 'http://localhost:3000/data',
+    url: 'http://localhost:3000/stream',
     event: 'counter'
 }, undefined, 0);
