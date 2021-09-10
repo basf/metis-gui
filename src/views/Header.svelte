609809bbbc47d9fe6a4dc69d72b1679075643ff8
Valexr <valexr@gmail.com>
2021-09-10 11:43:06 +0300

    integrations test in ./src/views/Header.svelte

--- src/views/Header.svelte
+++ src/views/Header.svelte
@@ -1,29 +1,33 @@
 <Navbar>
 	<nav slot="left">
 		{#if $path.length > 1 && $path.toString() !== '/login'}
-		<div class="ml-2">
-			<IconButton icon="icon-back" on:click={back}></IconButton>
-		</div>
+			<div class="ml-2">
+				<IconButton icon="icon-back" on:click={back} />
+			</div>
 		{/if}
-	</nav>	
+	</nav>
 
 	<svelte:fragment slot="center">
 		<img src="./b-science.png" width="64px" height="64px" alt="Brand-Name" />
+		<Button />
+		<IconButton icon="icon-people" iconSize="1x" href="#_" />
+		<Icon icon="icon-back" />
 	</svelte:fragment>
 
 	<nav slot="right">
 		{#if $user}
-		<div class="mr-2">
-			<IconButton icon="icon-people" href="/profile"></IconButton>
-		</div>
+			<div class="mr-2">
+				<IconButton icon="icon-people" href="/profile" />
+			</div>
 		{/if}
-	</nav>	
+	</nav>
 </Navbar>
 
 <script>
 	import { back, path } from 'svelte-pathfinder';
-	import { IconButton } from '@/components/Button';
-	import Navbar from '@/layouts/Navbar.svelte';
+	import { Navbar, Button, IconButton, Icon } from 'svelte-spectre';
+	// import Navbar from '@/layouts/Navbar.svelte';
+	// import { IconButton } from '@/components/Button';
+	// import Icon from '@/components/Icon';
 	import user from '@/stores/user';
-
 </script>
