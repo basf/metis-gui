8e2a729b882253f14afd4e25825b665b91066c50
Valexr <valexr@gmail.com>
2021-09-28 22:17:22 +0300

    Footer improve

--- src/views/Footer.svelte
+++ src/views/Footer.svelte
@@ -1,14 +1,18 @@
-<footer class="container grid-md text-center p-fixed">
-	<p>© {new Date().getFullYear()} <a href="https://basf.science">basf.science</a></p>
+<footer class="container text-center bg-gray d-flex p-fixed py-2">
+	<copy class="py-2">
+		© {new Date().getFullYear()} <a href="https://basf.science">basf.science</a>
+	</copy>
 </footer>
 
 <script lang="ts">
-	import { Container, Navbar, IconButton } from 'svelte-spectre';
+	// import { Container, Navbar, IconButton } from 'svelte-spectre';
 </script>
 
 <style lang="scss">
 	footer {
 		inset-inline: 0;
 		bottom: 0;
+		align-items: center;
+		justify-content: center;
 	}
 </style>
