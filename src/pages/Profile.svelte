6798a2564b99f0c1be7a63db941ea34ed98608a9
Valexr <valexr@gmail.com>
2021-09-28 22:04:20 +0300

    Fix Main layout

--- src/pages/Profile.svelte
+++ src/pages/Profile.svelte
@@ -1,36 +1,34 @@
-<Container>
-	<Grid>
-		<Col col="6" offset="mx-auto">
-			{#if $user}
-				<Panel>
-					<div slot="header" class="text-center">
-						<figure class="avatar avatar-lg" data-initial="AE" />
-						<div class="panel-title h5 mt-10">{$user.firstname} {$user.lastname}</div>
-					</div>
-					<nav slot="nav">
-						<!-- TODO: split to separate component -->
-						<ul class="tab tab-block">
-							<li class="tab-item active">Profile</li>
-						</ul>
-						<!-- / TODO: split to separate component -->
-					</nav>
-					<div slot="body">
-						<Tile>
-							<b slot="title">E-mail: {$user.email}</b>
-						</Tile>
-					</div>
+<Grid>
+	<Col col="6" offset="mx-auto">
+		{#if $user}
+			<Panel>
+				<div slot="header" class="text-center">
+					<figure class="avatar avatar-lg" data-initial="AE" />
+					<div class="panel-title h5 mt-10">{$user.firstname} {$user.lastname}</div>
+				</div>
+				<nav slot="nav">
+					<!-- TODO: split to separate component -->
+					<ul class="tab tab-block">
+						<li class="tab-item active">Profile</li>
+					</ul>
+					<!-- / TODO: split to separate component -->
+				</nav>
+				<div slot="body">
+					<Tile>
+						<b slot="title">E-mail: {$user.email}</b>
+					</Tile>
+				</div>
 
-					<svelte:fragment slot="footer">
-						<Button on:click={doLogout} variant="primary" block>Logout</Button>
-					</svelte:fragment>
-				</Panel>
-			{/if}
-		</Col>
-	</Grid>
-</Container>
+				<svelte:fragment slot="footer">
+					<Button on:click={doLogout} variant="primary" block>Logout</Button>
+				</svelte:fragment>
+			</Panel>
+		{/if}
+	</Col>
+</Grid>
 
 <script>
-	import { Button, Col, Container, Grid, Panel, Tile } from 'svelte-spectre';
+	import { Button, Col, Grid, Panel, Tile } from 'svelte-spectre';
 
 	import user from '@/stores/user';
 
