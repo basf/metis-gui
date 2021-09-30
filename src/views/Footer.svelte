ea7d5c531574e72f8f66cb96410547c0c57a28d5
Valexr <valexr@gmail.com>
2021-09-30 08:27:32 +0300

    stycky Footer

--- src/views/Footer.svelte
+++ src/views/Footer.svelte
@@ -1,4 +1,4 @@
-<footer class="container text-center bg-gray d-flex p-fixed py-2">
+<footer class="container text-center bg-gray d-flex py-2 mt-2">
 	<copy class="py-2">
 		© {new Date().getFullYear()} <a href="https://basf.science">basf.science</a>
 	</copy>
@@ -9,9 +9,31 @@
 
 <style lang="scss">
 	footer {
-		inset-inline: 0;
-		bottom: 0;
 		align-items: center;
 		justify-content: center;
+	}
+
+	/* sticky footer --------------------------- */
+	:global(body),
+	:global(html) {
+		height: 100%;
+
+		/* modern ------------------------------ */
+		:global(.spectre) {
+			min-height: 100%;
+			display: grid !important;
+			grid-template-rows: auto 1fr auto;
+		}
+
+		/* legacy ------------------------------- */
+		// display: flex;
+		// flex-direction: column;
+		// :global(header),
+		// footer {
+		// 	flex-shrink: 0;
+		// }
+		// :global(main) {
+		// 	flex-grow: 1;
+		// }
 	}
 </style>
