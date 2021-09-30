66e14a9536c860b3838886e38f53632ff65dfd3f
Valexr <valexr@gmail.com>
2021-09-30 22:57:31 +0300

    remove unreachable import/export

--- src/packages/svelte-spectre/src/lib/components/Progress/index.ts
+++ src/packages/svelte-spectre/src/lib/components/Progress/index.ts
@@ -1,5 +1,3 @@
 export * from './Progress.svelte';
 export { default } from './Progress.svelte';
 export { default as Progress } from './Progress.svelte';
-export { progress } from './progress'
-export type { Options } from './progress'
---
+++ src/packages/svelte-spectre/src/lib/layouts/Form/Form.svelte
@@ -0,0 +1,24 @@
+<form {...$$restProps} class:form-horizontal={horizontal} on:submit|preventDefault>
+	<slot />
+</form>
+
+<script context="module" lang="ts">
+</script>
+
+<script lang="ts">
+	export let horizontal: boolean = true;
+</script>
+
+<style lang="scss">
+	:global(.spectre) {
+		@import 'spectre.css/src/forms';
+		.form-horizontal {
+			padding: $layout-spacing 0;
+
+			:global(.form-group) {
+				display: flex;
+				flex-wrap: wrap;
+			}
+		}
+	}
+</style>
