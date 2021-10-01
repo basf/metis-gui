4a3d3334f1d4bffcc1de9bb25b4363ae3f64ad26
Valexr <valexr@gmail.com>
2021-10-01 20:23:51 +0300

    revert horizontal

--- src/packages/svelte-spectre/src/lib/layouts/Form/Form.svelte
+++ src/packages/svelte-spectre/src/lib/layouts/Form/Form.svelte
@@ -8,4 +8,19 @@
 
 <style lang="scss">
 	@import 'spectre.css/src/forms';
+
+	:global(.spectre) {
+		.form-horizontal {
+			padding: $layout-spacing 0;
+
+			:global(.form-group) {
+				display: flex;
+				flex-wrap: wrap;
+
+				&:not(:last-child) {
+					margin-bottom: $layout-spacing;
+				}
+			}
+		}
+	}
 </style>
