d5a6fb8935cc98adfc68b205186da47a512b4f31
Valexr <valexr@gmail.com>
2021-10-04 11:54:08 +0300

    add font folder

--- src/packages/svelte-spectre/src/routes/index.svelte
+++ src/packages/svelte-spectre/src/routes/index.svelte
@@ -215,9 +215,12 @@
 
 <script lang="ts">
 	let questions = [
-			{ id: 1, text: `В какой школе ты учился?` },
-			{ id: 2, text: `Девичья фамилия твоей мамы?` },
-			{ id: 3, text: `Любая другая персональная информация?` },
+			{ id: 1, text: `Where did you go to school?` },
+			{ id: 2, text: `What is your mother's name?` },
+			{
+				id: 3,
+				text: `What is another personal fact that an attacker could easily find with Google?`,
+			},
 		],
 		selected = 1,
 		radios = 1;
