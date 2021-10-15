9c3403b51294b0f610c7d46683c31b065ea3d432
Valexr <valexr@gmail.com>
2021-10-15 20:24:44 +0300

    remove errors & add toast

--- src/layouts/Main.svelte
+++ src/layouts/Main.svelte
@@ -1,11 +1,4 @@
 <Container offset="mt-2">
-	{#if $errors.length}
-		{#each $errors as error}
-			<Toast offset="my-2" toastItem={{ type: 'error', close: true }}>
-				{error.message || error}
-			</Toast>
-		{/each}
-	{/if}
 	<Panel>
 		<nav slot="nav">
 			<Tabs items={menu} active={$path.toString()} block />
@@ -19,8 +12,5 @@
 <script lang="ts">
 	import { path } from 'svelte-pathfinder';
 	import { Container, Panel, Tabs, Toast } from 'svelte-spectre';
-
-	import errors from '@/stores/errors';
-
 	import { menu } from '@/routes';
 </script>
