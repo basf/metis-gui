76f8119b594a39a696d850c9163fd7a5945d21c3
Valexr <valexr@gmail.com>
2021-12-23 14:43:46 +0300

    remove undeed code

--- src/layouts/Main.svelte
+++ src/layouts/Main.svelte
@@ -11,9 +11,4 @@
 	import { url, path } from 'svelte-pathfinder';
 	import { Panel, Tabs } from 'svelte-spectre';
 	import { menu } from '@/routes';
-
-	// $: active = $path.toString() === '/' ? 1 : $url;
-
-	$: console.log(items, $url, $path, items[1].path);
-	let items = menu.map((m) => ($path.toString() === m.path ? { ...m, path: $url } : m));
 </script>
