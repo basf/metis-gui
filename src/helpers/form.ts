c5d2f5406932f972fa9b14ed1f5a4018a0a2a639
Pavel Malyshev <pmalyshev@devexperts.com>
2022-01-17 20:44:33 +0300

    make submit event cancelable

--- src/helpers/form.ts
+++ src/helpers/form.ts
@@ -1,5 +1,5 @@
 import type { ChangeEvent } from '@/types/event';
 
 export function submitOnChange(e: ChangeEvent): void {
-    e.target.form && e.target.form.dispatchEvent(new SubmitEvent('submit'));
+    e.target.form && e.target.form.dispatchEvent(new SubmitEvent('submit', { cancelable: true }));
 }
