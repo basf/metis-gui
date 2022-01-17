0bed1cdea6d21d128cf693a7f7cf647c03ec6208
Valexr <valexr@gmail.com>
2022-01-17 18:11:25 +0300

    add limit for total

--- src/pages/Search.svelte
+++ src/pages/Search.svelte
@@ -177,8 +177,12 @@
 	}
 
 	// fix for provider MP from reduce data_returned per page
-	function setTotal(provider: Param, data: number, page: Param) {
-		return provider === 'mp' && data < total && page && page > 1 ? total : data;
+	function setTotal(provider: Param, data: number, page: Param, limit: number = 10000) {
+		const maxMin = (max: number, min: number, val: number) => Math.min(max, Math.max(min, val));
+
+		return provider === 'mp' && data < total && page && page > 1
+			? maxMin(limit, 1, total)
+			: maxMin(limit, 1, data);
 	}
 
 	function setPage(page: Param) {
