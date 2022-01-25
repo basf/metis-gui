bce3e6c9d11a812d626acaa9030d24e09aadcdb7
Valexr <valexr@gmail.com>
2022-01-25 21:28:26 +0300

    #120, #133

--- src/pages/Calculations.svelte
+++ src/pages/Calculations.svelte
@@ -1,25 +1,32 @@
 <Main>
 	{#each $calculations as calculation (calculation.id)}
 		<Tile>
-			<h5 class="mt-2" slot="title">{@html calculation.name}</h5>
-			<svelte:fragment slot="subtitle">
-				<small class="tile-subtitle text-gray">
-					{showTimestamp(calculation)}
-				</small>
-			</svelte:fragment>
-			<svelte:fragment slot="action">
-				<IconButton icon="cross" on:click={() => delCalculation(calculation.id)} />
-			</svelte:fragment>
+			<IconButton slot="icon" icon="edit" on:click={() => (open = !open)} />
+			<h5 slot="title" class="mt-2">{@html calculation.name}</h5>
+			<small slot="subtitle" class="tile-subtitle text-gray">
+				{showTimestamp(calculation)}
+			</small>
+			<IconButton
+				slot="action"
+				icon="cross"
+				on:click={() => delCalculation(calculation.id)}
+			/>
 		</Tile>
 	{:else}
 		<div class="text-center distant_msg">No calculations</div>
 	{/each}
 </Main>
 
+<Modal bind:open size="fs">
+	<h3 slot="header">Modal header</h3>
+	<div class="content">Modal content</div>
+	<p slot="footer">Modal footer</p>
+</Modal>
+
 <script lang="ts" context="module">
 	import { onMount } from 'svelte';
 
-	import { Tile, IconButton } from 'svelte-spectre';
+	import { IconButton, Modal, Tile } from 'svelte-spectre';
 
 	import Main from '@/layouts/Main.svelte';
 
@@ -30,6 +37,8 @@
 </script>
 
 <script lang="ts">
+	let open = false; // temp variable for example
+
 	onMount(async () => {
 		setTimeout(getCalculations);
 	});
