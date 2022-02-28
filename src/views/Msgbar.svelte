c988cbaaf19a089af17dc89155c8b07b40e81261
Pavel Malyshev <paul.malys@gmail.com>
2022-02-28 23:29:18 +0300

    #125 - fix login stream data intialization

--- src/views/Msgbar.svelte
+++ src/views/Msgbar.svelte
@@ -10,8 +10,6 @@
 <script lang="ts" context="module">
 	import { IconButton, Toaster, toast } from 'svelte-spectre';
 
-	import calculations from '@/stores/calculations';
-	import datasources from '@/stores/datasources';
 	import status from '@/stores/status';
 	import errors from '@/stores/errors';
 </script>
@@ -21,9 +19,4 @@
 		const msg = error.message || error;
 		toast.error({ msg, timeout: 4000, pos: 'top_right' });
 	});
-
-	$: $calculations.length &&
-		toast.primary({ msg: 'Calculations synced', timeout: 2000, pos: 'top_right' });
-	$: $datasources.length &&
-		toast.primary({ msg: 'Structures synced', timeout: 2000, pos: 'top_right' });
 </script>
