ff2bff299c8a6092f02870aeb3cae8065f97c3eb
Valexr <valexr@gmail.com>
2022-03-05 19:29:46 +0300

    remove undeed code

--- src/views/Msgbar.svelte
+++ src/views/Msgbar.svelte
@@ -10,34 +10,22 @@
 <script lang="ts" context="module">
 	import { IconButton, Toaster, toast } from 'svelte-spectre';
 	import user from '@/stores/user';
-
 	import status from '@/stores/status';
 	import errors from '@/stores/errors';
 </script>
 
 <script lang="ts">
-	import { onMount, afterUpdate, tick } from 'svelte';
-	let offline = false,
-		session: boolean = false;
+	let offline = false;
 
 	$: {
-		setTimeout(() => {
-			offline = !$status.online || !$status.heartbeat;
-			session = $user ? true : false;
-		}, 500);
+		setTimeout(() => (offline = !$status.online || !$status.heartbeat), 500);
 
-		if (offline && session) {
-			toast.warning({ msg: 'You are logged out', timeout: 4000, pos: 'top_right' });
-		}
+		if (offline && $user)
+			toast.warning({ msg: 'You are logged out', timeout: 0, pos: 'top_right' });
 
 		$errors.forEach(({ error }) => {
 			const msg = error.message || error;
 			toast.error({ msg, timeout: 4000, pos: 'top_right' });
 		});
 	}
-	// afterUpdate(() => {
-	// 	if (offline && session) {
-	// 		toast.warning({ msg: 'You are logged out', timeout: 0, pos: 'top_right' });
-	// 	}
-	// });
 </script>
