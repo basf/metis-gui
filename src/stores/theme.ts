e92d324ce0f9fe2da4c587273d0fec8689f3aa4f
Valexr <valexr@gmail.com>
2022-04-18 10:38:26 +0300

    $media store by @PaulMaly 👍🏻

--- src/stores/theme.ts
+++ src/stores/theme.ts
@@ -1,8 +1,8 @@
 import { writable, get } from 'svelte/store';
 import { getItem, setItem } from '@/helpers/storage';
-import { media, MediaQuery } from '@/stores/media';
+import { media, type Queries } from '@/stores/media';
 
 export const darkTheme = writable(
-    JSON.parse(getItem('xray_darkTheme', sessionStorage)) ?? get<MediaQuery>(media).dark
+	JSON.parse(getItem('xray_darkTheme', sessionStorage)) ?? get<Queries>(media).dark
 );
 darkTheme.subscribe((val) => setItem('xray_darkTheme', val, sessionStorage));
