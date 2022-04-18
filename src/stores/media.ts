32fade85c08393fd2447b57d55fcf0981d39242a
Valexr <valexr@gmail.com>
2022-04-18 11:07:19 +0300

    fix TS

--- src/stores/media.ts
+++ src/stores/media.ts
@@ -23,17 +23,17 @@ export const media: Readable<Queries> = mediaStore(queries)
 
 function mediaStore(queries: Queries = {}) {
 	return readable({}, (set) => {
-		let mqs = Object.entries(queries).reduce((mqs, [key, query]) => {
-			mqs[key] = window.matchMedia(query as string);
-			mqs[key].onchange = (mq: MediaQueryListEvent) => {
+		let mqs = Object.entries(queries).reduce((mqs: { [key: string]: MediaQueryListEvent }, [key, query]) => {
+			const media = window.matchMedia(query as string);
+			media.onchange = (mq: MediaQueryListEvent) => {
 				mqs[key] = mq;
 				update();
 			};
-			return mqs as Queries;
+			return mqs as { [key: string]: MediaQueryListEvent };
 		}, {});
 
 		function update() {
-			const matches: Queries = Object.entries(mqs as MediaQueryList).reduce((matches, [key, mq]) => {
+			const matches = Object.entries(mqs).reduce((matches: Queries, [key, mq]) => {
 				matches[key] = mq.matches;
 				return matches;
 			}, {});
@@ -45,4 +45,3 @@ function mediaStore(queries: Queries = {}) {
 		return () => mqs = {};
 	});
 }
-
