6f5dd4dd92f7de6e4e77045e72f9032a8805945e
Evgeny Blokhin <eb@tilde.pro>
2022-04-23 19:36:48 +0000

    Switch from *basf.science* to *metis.science*

--- src/config.ts
+++ src/config.ts
@@ -5,13 +5,13 @@ export const IS_FILE = location.protocol === 'file:';
 
 export const GUI_HOST = IS_FILE
 	? IS_PROD
-		? 'https://basf.science'
+		? 'https://metis.science'
 		: 'http://localhost:5000'
 	: location.origin;
 
 export const API_VERSION = 'v0';
 
-export const API_HOST = IS_PROD ? 'https://gate.basf.science' : 'http://localhost:3000';
+export const API_HOST = IS_PROD ? 'https://gate.metis.science' : 'http://localhost:3000';
 
 export const API_BASEURL = `${API_HOST}/${API_VERSION}`;
 
@@ -19,15 +19,13 @@ export const STREAM_URL = API_HOST + '/stream';
 
 export const BASE_PATH = '';
 
-export const SEARCH_DELAY = 1000;
 export const NEW_VERSION_CHECK_DELAY = 60000;
 
 export const OPTIMADE_PROVIDERS_URL = 'https://providers.optimade.org/providers.json';
-
 export const CORS_PROXY_URL = 'https://cors.optimade.science';
-
 export const OPTIMADE_PROVIDERS = ['mp', 'nmd', 'threedd', 'icdd-pdf-4-plus', 'ccdc'];
+export const SEARCH_DELAY = 1000;
 
 export const DOWNLOADABLE_APP_FILENAME = 'index.html';
 
-export const SYNC_TOASTS_CONFIG = { timeout: 2000, pos: 'top_right' } as ToastItem;
+export const SYNC_TOASTS_CONFIG = { timeout: 2000, pos: 'bottom_right' } as ToastItem;
