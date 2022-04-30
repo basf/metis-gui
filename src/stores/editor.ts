43c9f810f63de853bd6a8214e83d1277b28c8487
Valexr <valexr@gmail.com>
2022-04-30 06:54:42 +0300

    rename storage key

--- src/stores/editor.ts
+++ src/stores/editor.ts
@@ -3,6 +3,6 @@ import { getItem, setItem } from '@/helpers/storage';
 import type { Template } from '@/types/dto';
 
 export const editorCode = writable<Template>(
-	getItem('xray_EditorCode', sessionStorage) ?? '{}' as unknown as Template
+	getItem('xray_editorCode', sessionStorage) ?? '{}' as unknown as Template
 );
-editorCode.subscribe((val) => setItem('xray_EditorCode', val, sessionStorage));
+editorCode.subscribe((val) => setItem('xray_editorCode', val, sessionStorage));
