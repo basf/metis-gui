a3d876c33d2c2a93fd4dde797154c7f01aa5a0f0
Valexr <valexr@gmail.com>
2022-08-14 19:30:09 +0300

    add Input, Select

--- src/components/Editor/Form.svelte
+++ src/components/Editor/Form.svelte
@@ -1,14 +1,32 @@
 <Form>
 	<h5>{description}</h5>
-	<pre>
-		{#each Object.entries(properties) as [key, value] (key)}
-			{key}: {JSON.stringify(value, 0, 2)}
-		{/each}
-	</pre>
+	{#each Object.entries(properties).sort() as [key, value] (key)}
+		{#if value.properties && Object.keys(value.properties).length}
+			<fieldset>
+				<legend><h3>{key}</h3></legend>
+				{#each Object.entries(value.properties).sort() as [key, value]}
+					{key}
+					{#if value?.enum?.length || value?.items?.enum?.length}
+						<Select options={value.enum || value.items.enum} />
+					{/if}
+					<details class="text-gray">
+						<summary>JSON</summary>
+						<small><pre>{key}: {JSON.stringify(value, 0, 2)}</pre></small>
+					</details>
+				{/each}
+			</fieldset>
+		{:else}
+			<Input type={value.type} placeholder={value.description}>{key}</Input>
+			<details class="text-gray">
+				<summary>JSON</summary>
+				<small><pre>{key}: {JSON.stringify(value, 0, 2)}</pre></small>
+			</details>
+		{/if}
+	{/each}
 </Form>
 
 <script lang="ts" context="module">
-	import { Form } from 'svelte-spectre';
+	import { Form, Input, Select } from 'svelte-spectre';
 </script>
 
 <script lang="ts">
