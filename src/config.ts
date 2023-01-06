85aaef75e3c008683bff29214849effa0ff3ac69
Askar Yusupov <devex.soft@gmail.com>
2023-01-06 13:00:33 +0300

    feat: add env.ts, change config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,40 +1,32 @@
 import type { ToastItem } from 'svelte-spectre';
+import env from './env';
 
-export const RUNTIME_CONFIG = typeof window !== 'undefined' && window._METIS_RUNTIME_CONFIG || {};
+const IS_PROD = env.NODE_ENV === 'production';
+const IS_FILE = location.protocol === 'file:';
 
-export const IS_PROD = __env === 'production';
-export const IS_FILE = location.protocol === 'file:';
+const GUI_HOST =
+	env.GUI_HOST ||
+	(IS_FILE ? (IS_PROD ? 'https://metis.science' : 'http://localhost:5000') : location.origin);
 
-export const GUI_HOST = RUNTIME_CONFIG.GUI_HOST ||
-	(IS_FILE
-		? IS_PROD
-			? 'https://metis.science'
-			: 'http://localhost:5000'
-		: location.origin);
+const API_VERSION = 'v0';
 
-export const API_VERSION = 'v0';
+const API_BASEURL = `${env.API_HOST}/${API_VERSION}`;
 
-export const API_HOST = RUNTIME_CONFIG.API_HOST ||
-	(IS_PROD ? 'https://gate.metis.science' : 'http://localhost:3000');
+const STREAM_URL = env.API_HOST + '/stream';
 
-export const API_BASEURL = `${API_HOST}/${API_VERSION}`;
+const DOWNLOADABLE_APP_FILENAME = 'index.html';
 
-export const STREAM_URL = API_HOST + '/stream';
+const SYNC_TOASTS_CONFIG = { timeout: 2000, pos: 'bottom_right' } as ToastItem;
 
-export const BASE_PATH = RUNTIME_CONFIG.BASE_PATH || '';
-
-export const NEW_VERSION_CHECK_DELAY = RUNTIME_CONFIG.NEW_VERSION_CHECK_DELAY || 60000;
-
-export const VISUALIZER_URL = RUNTIME_CONFIG.VISUALIZER_URL || 'https://tilde-lab.github.io/cifplayer/player.html';
-export const OPTIMADE_PROVIDERS_URL = RUNTIME_CONFIG.OPTIMADE_PROVIDERS_URL || 'https://providers.optimade.org/providers.json';
-export const CORS_PROXY_URL = RUNTIME_CONFIG.CORS_PROXY_URL || 'https://cors.optimade.science';
-export const OPTIMADE_PROVIDERS = RUNTIME_CONFIG.OPTIMADE_PROVIDERS || ['mp', 'mc2d', 'mc3d', 'icdd-pdf-4-plus', 'ccdc']; // FIXME the last two are private
-export const SEARCH_DELAY = RUNTIME_CONFIG.SEARCH_DELAY || 1000;
-
-export const DOWNLOADABLE_APP_FILENAME = 'index.html';
-
-export const SYNC_TOASTS_CONFIG = { timeout: 2000, pos: 'bottom_right' } as ToastItem;
-
-export const IdPs = RUNTIME_CONFIG.IDPS || ['basf']; // available: mpds, github, linkedin, orcid, basf, local
-
-export const PAGE_LIMIT = RUNTIME_CONFIG.PAGE_LIMIT || 25;
+const config = {
+	...env,
+	IS_PROD,
+	IS_FILE,
+	GUI_HOST,
+	API_VERSION,
+	API_BASEURL,
+	STREAM_URL,
+	DOWNLOADABLE_APP_FILENAME,
+	SYNC_TOASTS_CONFIG,
+};
+export default config;
---
+++ src/env.ts
@@ -0,0 +1,43 @@
+import { parseEnv, z } from 'znv';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const env = parseEnv(process.env, {
+	NODE_ENV: z.enum(['production', 'development', 'test']).default('development'),
+	GUI_HOST: {
+		schema: z.string().url(),
+		defaults: {
+			production: 'https://metis.science',
+			_: 'http://localhost:5000',
+		},
+	},
+	API_HOST: {
+		schema: z.string().url(),
+		defaults: {
+			production: 'https://gate.metis.science',
+			_: 'http://localhost:3000',
+		},
+	},
+	BASE_PATH: z.string().default(''),
+	NEW_VERSION_CHECK_DELAY: z.number().int().default(60000),
+	VISUALIZER_URL: z.string().url().default('https://tilde-lab.github.io/cifplayer/player.html'),
+	OPTIMADE_PROVIDERS_URL: z
+		.string()
+		.url()
+		.default('https://providers.optimade.org/providers.json'),
+	CORS_PROXY_URL: z.string().url().default('https://cors.optimade.science'),
+	OPTIMADE_PROVIDERS: z
+		.string()
+		.array()
+		.default(['mp', 'mc2d', 'mc3d', 'icdd-pdf-4-plus', 'ccdc']), // FIXME the last two are private
+	SEARCH_DELAY: z.number().int().default(1000),
+
+	DOWNLOADABLE_APP_FILENAME: z.string().default('index.html'),
+
+	IdPs: z.string().array().default(['basf']), // available: mpds, github, linkedin, orcid, basf, local
+
+	PAGE_LIMIT: z.number().int().default(25),
+});
+
+export default env;
