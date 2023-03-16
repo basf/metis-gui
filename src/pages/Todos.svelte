5fa9e04487bf0adb11f9ff5a3301df09855926f6
Askar Yusupov <devex.soft@gmail.com>
2023-03-16 13:04:28 +0500

    feat: add Todos collections

--- src/pages/Todos.svelte
+++ src/pages/Todos.svelte
@@ -1,12 +1,28 @@
 <Main>
-	<Empty>
-		<Icon slot="icon" icon="refresh" size="3x" />
-		<h1 slot="title">Coming soon...</h1>
-		<p slot="subtitle">This section is under development</p>
-	</Empty>
+	<Section asyncData={$collectionsAsync}>
+		<svelte:fragment let:item>
+			{#if item.typeFlavor === 'red'}
+				<Col col="12">
+					<Collection
+						id="{item.id},"
+						title={item.title}
+						description={item.description}
+						visibility={item.visibility}
+						userId={item.userId}
+						userFirstName={item.userFirstName}
+						userLastName={item.userLastName}
+						typeFlavor={item.typeFlavor}
+					/>
+				</Col>
+			{/if}
+		</svelte:fragment>
+	</Section>
 </Main>
 
 <script lang="ts">
-	import { Empty, Icon } from 'svelte-spectre';
-	import { Main } from '@/layouts';
+	import { Col } from 'svelte-spectre';
+
+	import { Main, Section } from '@/layouts';
+	import { collectionsAsync } from '@/stores/collections';
+	import { Collection } from '@/views/tiles';
 </script>
