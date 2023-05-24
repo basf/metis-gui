946c0f46a5c2aa01e31f0edcb35be18dd1ece4c2
Sergei Korolev <knopki@duck.com>
2023-05-24 11:23:16 +0300

    build(60-runtime-config.sh): set permissions instead of copy

--- .docker/docker-entrypoint.d/60-runtime-cfg.sh
+++ .docker/docker-entrypoint.d/60-runtime-cfg.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -e pipefail
+set -euo pipefail
 
 : "${METIS_RUNTIME_CONFIG_SRC:=/srv/index.html}"
 : "${METIS_RUNTIME_CONFIG_DEST:=/srv/index.html}"
@@ -9,7 +9,8 @@ PREFIX="window._METIS_RUNTIME_CONFIG = "
 if [ -n "${METIS_RUNTIME_CONFIG+x}" ]; then
     echo "Injecting runtime config..."
     TMP=$(mktemp)
-    cp --attributes-only --preserve "$METIS_RUNTIME_CONFIG_SRC" "$TMP"
+    touch "$TMP"
+    chmod 644 "$TMP"
     sed -e "s/$PREFIX{}/$PREFIX\${METIS_RUNTIME_CONFIG}/" "$METIS_RUNTIME_CONFIG_SRC" \
         | envsubst \$METIS_RUNTIME_CONFIG > "$TMP"
     mv "$TMP" "$METIS_RUNTIME_CONFIG_DEST"
